cc9b9648ad8653e4c0ceb666246bd1b2b117d1a7
KOT DEVELOPERS <95179243+KOT-BOTS-REPO@users.noreply.github.com>
2021-11-28 21:30:47 +0530

    Kot.py
    More update by : @kot_bots

--- genStrSession.py
+++ genStrSession.py
@@ -1,1 +1,23 @@
 
+async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
+
+    async with Client(
+
+            "KOT_BOTS",
+
+            api_id=int(os.environ.get("APP_ID") or input("Enter Telegram APP ID: ")),
+
+            api_hash=os.environ.get("API_HASH") or input("Enter Telegram API HASH: "),
+
+            device_model="CodeXBotz Ver 1.0.1"
+
+    ) as codexbotz:
+
+        print("\nprocessing...")
+
+        await codexbotz.send_message(
+
+            "me", f"<b>@KOT_BOTS User Session</b>\n\n<code>{await codexbotz.export_session_string()}</code>\n\n<i>Keep this Safe and Not share anyone</i>")
+
+        print("Done!, Check your Saved Message for String Session!")
+
