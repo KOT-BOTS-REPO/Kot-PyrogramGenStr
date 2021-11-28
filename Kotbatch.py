b416497df8ac52377164e3ec0c3cbf4664a9dd2f
KOT DEVELOPERS <95179243+KOT-BOTS-REPO@users.noreply.github.com>
2021-11-28 21:55:41 +0530

    Update Kotbatch.py

--- Kotbatch.py
+++ Kotbatch.py
@@ -7,6 +7,7 @@
 
 <a href="https://t.me/KOT_BOTS">
   &nbsp:<img src="https://img.shields.io/badge/KOT%20%F0%9D%95%8F%20BOTS-channel-blue?style=flat-square&logo=telegram"width="130" height="18">&nbsp;
+  </a>
 
 
   
